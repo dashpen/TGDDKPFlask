754faae2b60f3a3c64191c6542fce629dcbf5c82
Gene Chang <genechang0219@gmail.com>
2023-02-15 21:25:58 -0800

    added login wireframe

---
+++ login.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, jsonify, request
+from flask_restful import Api, Resource 
+import requests   
+import json as JSON
+import ast
+from model_chess import check_password_hash
+from model_chess import getUser
+# Blueprints allow this code to be procedurally abstracted from main.py, meaning code is not all in one place
+server1 = Blueprint('balls', __name__,
+                   url_prefix='/api/server1')  # endpoint prefix avoid redundantly typing /api/jokes over and over
+
+# API generator https://flask-restful.readthedocs.io/en/latest/api.html#id1
+api = Api(server1)
+
+data = []
+
+class ChessAPI:
+    class _check(Resource):
+        def get(self):
+            body = request.json()
+            return check_password_hash
+
+
+
+
+                
+
+
+    # class _clear(Resource):
+
+
+    api.add_resource(_check, '/')
+
+if __name__ == "__main__": 
+    print("LMAO LOOSER!")
