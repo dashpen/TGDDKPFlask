ef478a3e872de58591a2c84ef7a48992d87b9ddc
dashpen <dashpenning@gmail.com>
2023-02-15 19:50:32 -0800

    change

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_cors import CORS
+# from flask_cors import CORS
 
 """
 These object can be used throughout project.
@@ -12,7 +12,7 @@ These object can be used throughout project.
 
 # Setup of key Flask object (app)
 app = Flask(__name__)
-cors = CORS(app)
+# CORS(app, origins=['http://localhost:5000', 'https://genechang1.github.io'])
 # Setup SQLAlchemy object and properties for the database (db)
 dbURI = 'sqlite:///volumes/sqlite.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
