9e072693b47fe50e9160fabee7a48e08fdb47522
paravsalaniwal <paravsalaniwal@gmail.com>
2023-02-15 21:29:16 -0800

    battleship

--- battleship.py
+++ battleship.py
@@ -22,23 +22,8 @@ class UserAPI:
             if name is None or len(name) < 2:
                 return {'message': f'Name is missing, or is less than 2 characters'}, 210
 
-            password = body.get('password')
-            dob = body.get('dob')
-
             ''' #1: Key code block, setup USER OBJECT '''
             uo = BattleshipUsers(name=name)
-            
-            ''' Additional garbage error checking '''
-            # set password if provided
-            if password is not None:
-                uo.set_password(password)
-            # convert to date type
-            if dob is not None:
-                try:
-                    uo.dob = datetime.strptime(dob, '%m-%d-%Y').date()
-                except:
-                    return {'message': f'Date of birth format error {dob}, must be mm-dd-yyyy'}, 210
-            
             ''' #2: Key Code block to add user to database '''
             # create user in database
             uo.create()
@@ -46,12 +31,18 @@ class UserAPI:
             if uo:
                 return jsonify(uo.read())
             # failure returns error
-            return {'message': f'Processed {name}, either a format error or User ID {uo.uid} is duplicate'}, 210
+            return {'message': f'Processed {name}, either a format error or User ID {uo.username} is duplicate'}, 210
 
     class _Read(Resource):
         def get(self):
             users = BattleshipUsers.query.all()    # read/extract all users from database
             json_ready = [user.read() for user in users]  # prepare output in json
+            return jsonify(json_ready)  # jsonify creates Flask response object, more specific to APIs than json.dumps
+
+    class _Read(Resource):
+        def get(self):
+            scores = BattleshipUsers.query.all()    # read/extract all users from database
+            json_ready = [score.read() for score in scores]  # prepare output in json
             return jsonify(json_ready)  # jsonify creates Flask response object, more specific to APIs than json.dumps
 
     # building RESTapi endpoint
