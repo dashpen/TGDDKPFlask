81c1a0c8e4004c9b96b2d841e67396126eb1502b
Gene Chang <genechang0219@gmail.com>
2023-02-27 23:50:20 -0800

    ispasswordmatchispasswordmatch

--- login.py
+++ login.py
@@ -6,6 +6,7 @@ import ast
 from model_chess import check_password_hash
 from model_chess import getUser
 from model_chess import ChessUsers
+from model_chess import is_password_match
 # Blueprints allow this code to be procedurally abstracted from main.py, meaning code is not all in one place
 NameAPI = Blueprint('NameAPI', __name__,
                    url_prefix='/api/names')  # endpoint prefix avoid redundantly typing /api/jokes over and over
@@ -20,7 +21,7 @@ class _Read(Resource):
                 return {'message': f'Name is missing, or is less than 2 characters'}, 400
             password = body.get('password')
             user = ChessUsers.query.filter_by(_name=name).first()
-            if user is None or not user.is_password(password):
+            if user is None or not user.is_password_match(password):
                 return {'message': f"Invalid user id or password"}, 400
             return jsonify(user.read())
             
