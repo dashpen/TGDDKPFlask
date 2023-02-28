fcd188e7b92324e442ae166fd374294a0773b964
Gene Chang <genechang0219@gmail.com>
2023-02-28 01:25:14 -0800

    help

--- login.py
+++ login.py
@@ -5,14 +5,17 @@ import json as JSON
 import ast
 from model_chess import getName
 from model_chess import ChessUsers
+from flask_cors import CORS # import the CORS module
 # Blueprints allow this code to be procedurally abstracted from main.py, meaning code is not all in one place
 NameAPI = Blueprint('NameAPI', __name__,
                    url_prefix='/api/names')  # endpoint prefix avoid redundantly typing /api/jokes over and over
 
 # API generator https://flask-restful.readthedocs.io/en/latest/api.html#id1
 api = Api(NameAPI)
+
+CORS(api)
 class _Read(Resource):
-        def get(self):
+        def post(self):
             body = request.get_json(force=True)
             name = body.get('name')
             if name is None or len(name) < 2:
