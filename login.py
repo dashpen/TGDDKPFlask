23977363a3a2c666025beb79f4fef4c5975087dc
Gene Chang <genechang0219@gmail.com>
2023-02-28 02:52:34 -0800

    * compact *

--- login.py
+++ login.py
@@ -1,15 +1,9 @@
 from flask import Blueprint, jsonify, request
-from flask_restful import Api, Resource 
-import requests   
-import json as JSON
-import ast
-from model_chess import getName
-from model_chess import ChessUsers
-
+from flask_restful import Api, Resource
+from model_chess import getName, ChessUsers
 
 # Blueprints allow this code to be procedurally abstracted from main.py, meaning code is not all in one place
-NameAPI = Blueprint('NameAPI', __name__,
-                   url_prefix='/api/names')  # endpoint prefix avoid redundantly typing /api/jokes over and over
+NameAPI = Blueprint('NameAPI', __name__, url_prefix='/api/names')
 
 # API generator https://flask-restful.readthedocs.io/en/latest/api.html#id1
 api = Api(NameAPI)
@@ -29,10 +23,10 @@ class Login(Resource):
         password = body.get('password')
         user = getName(name)
         # user = ChessUsers.query.filter_by(_name=name).first()
-             
+
         if user is None:
             return {'message': f"Invalid user id or password"}, 400
-        
+
         if not user.is_password_match(password):
             return {'message': f"wrong password"}, 400
         response = jsonify(user.read())
