21638e3dada50344da99b84ec7327368472c5d61
Gene Chang <genechang0219@gmail.com>
2023-04-30 16:54:36 -0700

    dd

--- login.py
+++ login.py
@@ -2,15 +2,13 @@ from flask import Blueprint, jsonify, request
 from flask_restful import Api, Resource
 from model_chess import getName, ChessUsers
 
-# Blueprints allow this code to be procedurally abstracted from main.py, meaning code is not all in one place
+
 NameAPI = Blueprint('NameAPI', __name__, url_prefix='/api/names/')
 
-# API generator https://flask-restful.readthedocs.io/en/latest/api.html#id1
 api = Api(NameAPI)
 
-# Add CORS support to the NameAPI blueprint
+
 def after_request(response):
-    # Add the 'Access-Control-Allow-Origin' and 'Access-Control-Allow-Headers' headers to the response
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     return response
 
